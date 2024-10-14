e2c21f7cc36d99698b6f445bf6f077e41aefb0de
peuborg <peuborg.8@gmail.com>
2024-10-14 09:46:37 -0300

    pre aula 09

--- Aula09/cep.js
+++ Aula09/cep.js
@@ -1,84 +1,57 @@
 function consultaCep() {
 
     var cep = document.getElementById('txtcep').value
+    var tblcep = document.getElementById("tblcep").querySelector("tbody")
+    tblcep.innerHTML = ''
+
+    if (cep.length !== 8) {
+        alert('CEP inválido!')
+        return
+    }
+
     var url = `https://viacep.com.br/ws/${cep}/json/`
-    var request = new XMLHttpRequest()
 
-    request.open('GET', url)
-    request.onerror = function (e) {
-        document.getElementById('divresultado').innerHTML = 'API offline ou CEP inválido'
+    fetch(url)
+        .then(response => {
+            console.log(response)
+            if (!response.ok) {
+                throw new Error("CEP não encontrado!")
+            }
+            return response.json()
+        })
+        .then(data => {
+            console.log(data)
+            mostrarEndereco(data)/*leitura do JSON data*/
+        })
+        .catch(error => {
+            console.error('Erro:', error)
+        })
+}
+
+function mostrarEndereco(dados) {
+    if (dados.erro) {
+        alert("CEP não encontrado!")
+    } else {
+        //Logradouro
+        inserirLinhaTable(`Logradouro`, `${dados.logradouro}`)
+        //Bairro
+        inserirLinhaTable(`Bairro`, `${dados.bairro}`)
+        //Cidade/UF
+        inserirLinhaTable(`Cidade/UF`, `${dados.localidade} / ${dados.uf}`)
+        //CEP
+        inserirLinhaTable(`CEP`, `${dados.cep}`)
     }
+}
 
-    request.onload = () => {
-        var tblcep = document.getElementById("tblcep").querySelector("tbody")
-        tblcep.innerHTML = ''
-        var response = JSON.parse(request.responseText)
-        
-        console.log(request.responseText)
-        
-        if (response.erro === true) {
-            document.getElementById('divresultado').innerHTML = 'CEP não encontrado'
-        } else {
-            /*document.getElementById('divresultado').innerHTML = `CEP: ${response.cep} <BR>` +
-                `Logradouro: ${response.logradouro} <BR>` +
-                `Bairro: ${response.bairro} <BR>` +
-                `CIDADE/UF: ${response.localidade}` +
-                `/ ${response.uf} <BR>`*/
-            document.getElementById('divresultado').innerHTML = ''
-            var coluna1 = document.createElement('td')
-            var coluna2 = document.createElement('td')
-            var linha = document.createElement('tr')
-            
-            //var contador = tblsimulacao.childElementCount
+function inserirLinhaTable(c1, c2) {
+    let coluna1 = document.createElement('td')
+    let coluna2 = document.createElement('td')
+    let linha = document.createElement('tr')
 
-            //CEP
-            coluna1 = document.createElement('td')
-            coluna2 = document.createElement('td')
-            linha = document.createElement('tr')
+    coluna1.innerHTML = c1
+    coluna2.innerHTML = c2
 
-            coluna1.innerHTML = `CEP`
-            coluna2.innerHTML = `${response.cep}`
-
-            linha.appendChild(coluna1)
-            linha.appendChild(coluna2)
-            tblcep.appendChild(linha)
-
-            //Logradouro
-            coluna1 = document.createElement('td')
-            coluna2 = document.createElement('td')
-            linha = document.createElement('tr')
-
-            coluna1.innerHTML = `Logradouro`
-            coluna2.innerHTML = `${response.logradouro}`
-
-            linha.appendChild(coluna1)
-            linha.appendChild(coluna2)
-            tblcep.appendChild(linha)
-
-            //Bairro
-            coluna1 = document.createElement('td')
-            coluna2 = document.createElement('td')
-            linha = document.createElement('tr')
-
-            coluna1.innerHTML = `Bairro`
-            coluna2.innerHTML = `${response.bairro}`
-
-            linha.appendChild(coluna1)
-            linha.appendChild(coluna2)
-            tblcep.appendChild(linha)
-
-            //Cidade/UF
-            coluna1 = document.createElement('td')
-            coluna2 = document.createElement('td')
-            linha = document.createElement('tr')
-
-            coluna1.innerHTML = `Cidade/UF`
-            coluna2.innerHTML = `${response.localidade} / ${response.uf}`
-
-            linha.appendChild(coluna1)
-            linha.appendChild(coluna2)
-            tblcep.appendChild(linha)
-        }
-    }
-    request.send()
+    linha.appendChild(coluna1)
+    linha.appendChild(coluna2)
+    tblcep.appendChild(linha)
 }
